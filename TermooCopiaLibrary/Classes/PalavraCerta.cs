40decf25387ca69349aa1bbe2de31189c4f255cb
Thalis Ribeiro <thalis199@outlook.com>
2022-02-26 18:02:35 -0300

    Atualização da classe com metodos para verificar se a letra digitada esta no mesmo lugar que a letra correta

--- TermooCopiaLibrary/Classes/PalavraCerta.cs
+++ TermooCopiaLibrary/Classes/PalavraCerta.cs
@@ -22,5 +22,58 @@ namespace TermooCopiaLibrary.Classes
         }
 
 
+
+        private Cores Recursivo(int indexRecebido ,List<int> listaIndex, int i =0)
+        {
+            Cores cor;
+            if (indexRecebido == listaIndex[i])
+            {
+                Letra disposableLetra = this.letras[indexRecebido];
+                cor = Cores.Green;
+                disposableLetra.cor = cor;
+                this.letras[indexRecebido] = disposableLetra;
+            }
+            else if (listaIndex.Count==i-1)
+            {
+                Letra disposableLetra = this.letras[indexRecebido];
+                cor = Cores.Yellow;
+                disposableLetra.cor = Cores.Yellow;
+                this.letras[indexRecebido] = disposableLetra;
+            }
+            else
+            {
+                cor = Recursivo(indexRecebido, listaIndex, i + 1);
+            }
+            return cor;
+        }
+
+        public List<Letra> VerificaAcerto(List<Letra> letrasRecebida)
+        {
+            
+            
+            for (int i = 0; i < 5; i++)
+            {
+                switch (ContemLetras(letrasRecebida[i].caracterie))
+                {
+                    case true:
+
+                        Letra disposableLetra = letrasRecebida[i];
+                        disposableLetra.cor = Recursivo(i,BuscaIguais(letrasRecebida[i].caracterie));
+                        letrasRecebida[i] = disposableLetra;
+                        break;
+                    default:
+
+                        Letra disposableLetra2 = letrasRecebida[i];
+                        disposableLetra.cor = Cores.Red;
+                        letrasRecebida[i] = disposableLetra2;
+                        break;
+                }
+            }
+            /*Letra disposableLetra = new();
+
+            disposableLetra.caracterie = letras[0].caracterie;
+            */
+            return letrasRecebida;
+        }
     }
 }
