2e118f138919935827b0c81392b5f08a8b8a9db3
Thalis Ribeiro <thalis199@outlook.com>
2022-02-26 19:48:33 -0300

    Adição de metodos para rodar o jogo

--- TermooCopiaLibrary/Classes/RepositorioCertas.cs
+++ TermooCopiaLibrary/Classes/RepositorioCertas.cs
@@ -8,11 +8,12 @@ namespace TermooCopiaLibrary.Classes
 {
     public class RepositorioCertas
     {
-        public static List<PalavraCerta> palavras = new();
 
+        public static List<PalavraCerta> palavras = new();
+        private int i;
         public void AdicionaPalavra(string palavraRecebida)
         {
-            if (palavraRecebida.Count()==5)
+            if (palavraRecebida.Length == 5)
             {
                 PalavraCerta palavra= new(palavraRecebida);
                 
@@ -20,7 +21,33 @@ namespace TermooCopiaLibrary.Classes
             }
         }
 
+        private void EscolhePalavra()
+        {
+            bool usada;
+            i = 0; do
+            {
+                usada = palavras[i].usada;
+                if (usada== true)
+                {
+                    i += 1;
+                }
+            } while (usada == true);
+            
+        }
 
+        string VerificaPalavra(PalavraEnviada enviada)
+        {
+            var vitoria = enviada.VerificaVitoria(palavras[i].VerificaAcerto(enviada.EnviaPalavra()));
+            if (vitoria == true)
+                return "Parabens, voce acertou";
+            else
+                return "Tente mais uma vez";
+        }
+        public string RecebePalavra(string palavraRecebida)
+        {
+            PalavraEnviada enviada = new(palavraRecebida);
+            return VerificaPalavra(enviada);
+        }
 
     }
 }
