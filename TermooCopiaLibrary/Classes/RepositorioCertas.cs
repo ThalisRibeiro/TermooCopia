90ec10b5a5a070e9a35890cd5f1eae96baabc2a7
Thalis Ribeiro <thalis199@outlook.com>
2022-02-26 19:17:04 -0300

    Criação do repositorio com metodo de adicionar nova palavra

---
+++ TermooCopiaLibrary/Classes/RepositorioCertas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermooCopiaLibrary.Classes
+{
+    public class RepositorioCertas
+    {
+        public static List<PalavraCerta> palavras = new();
+
+        public void AdicionaPalavra(string palavraRecebida)
+        {
+            if (palavraRecebida.Count()==5)
+            {
+                PalavraCerta palavra= new(palavraRecebida);
+                
+                palavras.Add(palavra);
+            }
+        }
+
+
+
+    }
+}
